0ae351e7b3f3170d445316223f08f43de68fa6d7
jnorgini <jnorgini@gmail.com>
2023-07-15 02:06:16 -0300

    base url

--- frontend/src/util/system.ts
+++ frontend/src/util/system.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ?? "https://dslist-177j.onrender.com";
 
